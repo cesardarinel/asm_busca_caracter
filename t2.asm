ab5b394ed3ec04b46f48551dcc88234922bfc2a5
CesarDarinel <cesardarinelortiz@gmail.com>
2018-09-26 09:03:42 -0400

    actualizando la documentacion
    documentando

--- t2.asm
+++ t2.asm
@@ -5,87 +5,87 @@
 ;
 
 .model small
-.stack 512
+.stack 256
 .data
 ;========================Variables declaradas aqui===========================
-letrero DB 'INSERTE UNA CADENA NO MAS DE 50 CARACTERES-->: $'	                ; Cadena a desplegar
-pedirvariable DB 'INSERTE LA VARIABLE A BUSCAR : $'	; Cadena a desplegar
-text DB 'EL CARACTER A BUSCAR ES : $'	        ; Cadena a desplegar
-text2 DB 'Y SE REPITE : $'	        ; Cadena a desplegar
-arregloConDatos db 51 dup (0)		                ; buffer de lectura de cadena
-variableBuscada db 1 dup (0)			            ; buffer de lectura de cadena
-count db 1 dup (2)
-segundodigito db 1 dup (2)			                        ; buffer de lectura de cadena
+letrero DB 'INSERTE UNA CADENA NO MAS DE 50 CARACTERES-->: $'	; Cadena a desplegar
+pedirvariable DB 'INSERTE LA VARIABLE A BUSCAR : $'		; Cadena a desplegar
+text DB 'EL CARACTER A BUSCAR ES : $'	        		; Cadena a desplegar
+text2 DB 'Y SE REPITE : $'	        			; Cadena a desplegar
+arregloConDatos db 51 dup (0)		                	; buffer de lectura de cadena
+variableBuscada db 1 dup (0)			            	; buffer de lectura de cadena
+count db 1 dup (2)						;
+segundodigito db 1 dup (2)			                ; buffer de lectura de cadena
 .code
 ;============================================================================
 inicio:	
-			mov ax,@data
-			mov ds,ax
+			mov ax,@data			;	
+			mov ds,ax			;
 			mov es,ax                  	;set segment register
 			and sp,not 3               	;align stack to avoid AC fault
 ;====================================Código==================================  
 
-			mov ah,09				        ;Para mostrar en pantalla una cadena
-			mov dx, offset letrero			;posición de la cadena a montar
-			int 21h                         ; llamo al sistema 
+			mov ah,09			;Para mostrar en pantalla una cadena
+			mov dx, offset letrero		;posición de la cadena a montar
+			int 21h                         ;llamo al sistema 
 
-			mov bx,00						; no se si se necesita pero programacion fucional inicio todo en cero
+			mov bx,00			;no se si se necesita pero programacion fucional inicio todo en cero
 ciclodelectura:
-			mov ah,01						;para lectura de teclado.
-			int 21h							;llamada al SO
-			cmp al,13						;verifico si se pulsó el Enter.
-			je imprimoLetreroCaptura
-			mov arregloConDatos[bx],al		;copio el caracter tomado en el buffer.
-			cmp bx,50						;verifico si debo de salir.
-			je imprimoLetreroCaptura                       
-			inc bx							;apunto a la sgte. posición del buffer.
-			jmp ciclodelectura
+			mov ah,01			;para lectura de teclado.
+			int 21h				;llamada al SO
+			cmp al,13			;verifico si se pulsó el Enter.
+			je imprimoLetreroCaptura	;
+			mov arregloConDatos[bx],al	;copio el caracter tomado en el buffer.
+			cmp bx,50			;verifico si debo de salir.
+			je imprimoLetreroCaptura        ;si escribimos mas de 50 caracteres dejo de leer               
+			inc bx				;apunto a la sgte. posición del buffer.
+			jmp ciclodelectura		; continuo leyendo
 
 imprimoLetreroCaptura:
 
-			mov ah,09				        ;Para mostrar en pantalla una cadena
+			mov ah,09			;Para mostrar en pantalla una cadena
 			mov dx, offset pedirvariable	;posición de la cadena a montar
-			int 21h
+			int 21h				;llamada al sistema
 
-			mov ah,01					    ;para lectura de teclado.
-			int 21h							;llamada al SO
-			mov variableBuscada,al
+			mov ah,01			;para lectura de teclado.
+			int 21h				;llamada al SO
+			mov variableBuscada,al		;
 
-			mov bx,00						; no se si se necesita pero programacion fucional inicio todo en cero
-			mov count,00				    ; sino inicializando cero
-			mov al,variableBuscada			;muevo a al para comparar 
+			mov bx,00			;no se si se necesita pero programacion fucional inicio todo en cero
+			mov count,00			;sino inicializando cero
+			mov al,variableBuscada		;muevo a al para comparar 
 ciclodeconteo:
-			cmp arregloConDatos[bx],al;
-			jne nocuento
-			inc count
-	nocuento:
-			cmp bx,50						;verifico si debo de salir.
-			je letreroFinal                       
-			inc bx							;apunto a la sgte. posición del buffer.
-			jmp ciclodeconteo
+			cmp arregloConDatos[bx],al;	;comparo el monto con la variable a buscar
+			jne nocuento			;si no son iguales no cuento 
+			inc count			;si son iguales cuento lavariable
+	nocuento:					
+			cmp bx,50			;verifico si debo de salir.
+			je letreroFinal                 ;salimos del cilo y vamos al final     
+			inc bx				;apunto a la sgte. posición del buffer.
+			jmp ciclodeconteo		;retornamos al ciclo	
 
 letreroFinal:
-			mov al,count
-			aam ; separo
-			mov count,ah
-			mov segundodigito,al
+			mov al,count			; paso el monto a al para separarlos
+			aam 				;separo el monto de al en lamitas en ah y la otra en al	
+			mov count,ah			;muevo el monto de ah a la variable a presentar
+			mov segundodigito,al		;muevo el monto de al a la variable a presentar
 
-			mov ah,02						;para mostrar caracter
+			mov ah,02			;para mostrar caracter
 			mov dl,10                       ;imprimo el caracter de salto de linea 
 			int 21h                         ;llamada sistema 
 			
-			mov ah,09				        ;Para mostrar en pantalla una cadena
-			mov dx, offset text2	;posición de la cadena a montar
-			int 21h
+			mov ah,09			;Para mostrar en pantalla una cadena
+			mov dx, offset text2	        ;posición de la cadena a montar
+			int 21h               		;Llamada al sistema 
 
-			add count,48
-			mov ah,02						;para mostrar caracter
-			mov dl,count                       ;imprimo el caracter de salto de linea 
-			int 21h
+			add count,48			;sumo 48 para que pase a la tabla ascii
+			mov ah,02			;para mostrar caracter
+			mov dl,count                    ;imprimo el caracter de salto de linea 
+			int 21h				;Llamada al sistema
 
-			add segundodigito,48
-			mov ah,02						;para mostrar caracter
-			mov dl,segundodigito                       ;imprimo el caracter de salto de linea 
+			add segundodigito,48            ;sumo 48 para que pase a la tabla ascii
+			mov ah,02			;para mostrar caracter
+			mov dl,segundodigito            ;imprimo el caracter de salto de linea 
 			int 21h
 			
 
